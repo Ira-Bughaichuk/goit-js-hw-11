fec1fed0138f8521d6dc2d57b4b213441cab09d0
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2023-01-13 12:16:34 +0200

    hw 11-base

--- src/index.js
+++ src/index.js
@@ -1,41 +1,91 @@
 
 import {  getGallery} from "./js/galleryAPI";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
     formEl: document.querySelector('#search-form'),
-    listEl: document.querySelector('.gallery'),
+  listEl: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector(".load-more"),
 }
 refs.formEl.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onMoreLoadPage);
 
+let value = "";
+let currentPage = 1;
+let currentPer_page = 40;
 
-
-async function onSearch(e) {
-    e.preventDefault();
-   
-    
-    
+function onSearch(e) {
+  e.preventDefault();
     value = e.target.elements.searchQuery.value;
-    console.log(value);
-    
+ // console.log(value);
+  
    // getGallery(value).then(data => console.log(data.data.hits));
     //console.log(getGallery);
-    clearContainer();
-    try {
-        const card = await getGallery(value).then(res => {
-           // console.log(data.data.hits);
-            renderCard(res.data.hits);
-        })
-        
-        
-    } catch (error) {
-       // console.log("error")
-    }
-      
     
+    // try {
+    //     const card = await getGallery(value).then(res => {
+    //        // console.log(res.data.hits);
+    //       if (res.data.hits.length > 0 && checkSpaces(value)) {
+    //         renderCard(res.data.hits);
+    //       } else {
+    //         throw new Error('404')
+    //       }
+    //     }) 
+    // } catch (error) {
+    //    console.log("Sorry, there are no images matching your search query. Please try again.")
+    // }
     
+   refs.loadMoreBtn.hidden = false;
+ currentPage = 1;
+  e.target.reset();
+ clearContainer();
+  getCard(value);
+
+  
+  
+  //refs.loadMoreBtn.disabled = false;    
+
     // console.log(galleryApi);
     // if (galleryApi.value === '')
     //     return console.log("Sorry, there are no images matching your search query. Please try again.") 
+}
+async function getCard(value) {
+   try {
+        const res = await getGallery(value, currentPage)
+            //console.log(res.data.hits);
+          //console.log(res.data.totalHits);
+          
+          if (res.data.hits.length > 0 && checkSpaces(value)) {
+            renderCard(res.data.hits);
+            return res;
+          } else {
+            throw new Error('404');
+          }
+        
+    } catch (error) {
+     Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+     
+  }
+  return {}
+}
+function onMoreLoadPage() {
+  currentPage += 1;
+  
+
+  getCard(value).then((res) => {
+    console.log(res);
+    let total_pages = res.data.totalHits / currentPer_page;
+   
+    Notify.success(`Hooray! We found ${res.data.totalHits} images.`);
+    // console.log(currentPage);
+    // console.log(total_pages);
+    if (currentPage >= total_pages) {
+      refs.loadMoreBtn.hidden = true;
+      Notify.failure("We're sorry, but you've reached the end of search results.");
+    }
+  })
+ 
+ 
 }
 
 function renderCard(hits) {
@@ -44,21 +94,22 @@ function renderCard(hits) {
   <img src="${webformatURL}" data-src="${largeImageURL}" alt="${tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
-      <b>${likes}</b>
+      <b>Likes:</b> ${likes}
     </p>
     <p class="info-item">
-      <b>${views}</b>
+      <b>Views:</b> ${views}
     </p>
     <p class="info-item">
-      <b>${comments}</b>
+      <b>Comments:</b> ${comments}
     </p>
     <p class="info-item">
-      <b>${downloads}</b>
+      <b>Downloads:</b> ${downloads}
     </p>
   </div>
-</div>`
+  </div>`
             
     })).join('');
+  
     refs.listEl.insertAdjacentHTML('beforeend', markup);
 }
 function clearContainer() {
--- src/js/galleryAPI.js
+++ src/js/galleryAPI.js
@@ -4,21 +4,19 @@ const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '32790565-383584a211a893fe9ad088e3f';
 
 
- export function getGallery(value) {
+ export function getGallery(value, page, per_page) {
         const config = {
                 key: API_KEY,
                 q: value,
                 image_type: "photo",
                 orientation: "horizontal",
             safesearch: "true",
-            page: 1,
-            per_page: 40,
+            page: page,
+            per_page: per_page,
             
         }
      const response = axios.get(`${BASE_URL}`,{ params: config });
-        return response;
-
-       
+     return response;
     } 
 
     // incrementPage() {
